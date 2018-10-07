c29ba93a1371276c4a941a4955d0e412bac9114e
snkoski <shawnnkoski@gmail.com>
2018-10-07 12:35:18 -0700

    remove friendships from db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_192246) do
+ActiveRecord::Schema.define(version: 2018_09_20_160646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "friend_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
