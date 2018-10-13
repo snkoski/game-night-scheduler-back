b69a4db758e67b15b995a193cb779f975bf1b43b
snkoski <shawnnkoski@gmail.com>
2018-10-13 10:30:46 -0400

    Add description:text column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_045341) do
+ActiveRecord::Schema.define(version: 2018_10_13_142956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_13_045341) do
     t.string "location"
     t.integer "max_users"
     t.integer "current_users", default: 0
+    t.text "description"
   end
 
   create_table "games", force: :cascade do |t|
