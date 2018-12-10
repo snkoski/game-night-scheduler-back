de15c5bec8b493f8b57351033d3a4f5b5131e31b
snkoski <shawnnkoski@gmail.com>
2018-12-09 21:15:27 -0500

    game_hash touch up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :games, through: :user_games
   has_many :user_groups
   has_many :groups, through: :user_groups
-  has_many :friendships
-  has_many :friends, through: :friendships
+  # has_many :friendships
+  # has_many :friends, through: :friendships
   has_many :user_events
   has_many :events, through: :user_events
   has_many :votes
@@ -39,6 +39,7 @@ class User < ApplicationRecord
   end
 
   # private
+  
   def games_hash(bgg_username)
     games_hash = {}
     loop do
